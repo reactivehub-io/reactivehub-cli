e7057caaca69d40d18b700fd668a320a6ccec606
André Farias <andrerfarias@gmail.com>
2019-03-15 22:24:55 -0300

    event questions switch and better description

--- src/commands/questions/event.js
+++ src/commands/questions/event.js
@@ -3,14 +3,14 @@ import { idPatternValidator } from '../../validators'
 const create = [
   {
     type: 'input',
-    name: 'id',
-    message: 'Enter the event id ...',
+    name: 'eventGroup',
+    message: 'The event scope (e.g payment)  ...',
     validate: input => idPatternValidator(input),
   },
   {
     type: 'input',
-    name: 'eventGroup',
-    message: 'Enter the event group name ...',
+    name: 'id',
+    message: 'Enter the event id (e.g new-credit-card, this will create the composed event enpoint, for example: payment.new-credit-card)',
     validate: input => idPatternValidator(input),
   },
 ]
